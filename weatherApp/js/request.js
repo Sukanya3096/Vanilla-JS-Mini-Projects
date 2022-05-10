bc3c7bcc65da512205d51bf2fe7f0abf730eed10
Sukanya3096 <isukanyadutta@gmail.com>
2022-05-10 14:48:49 +0530

    encapsulation

--- weatherApp/js/request.js
+++ weatherApp/js/request.js
@@ -1,9 +1,10 @@
-const key = 'f17daeaeca054442cd33e6f9028cb8a3';
+import WEATHER_API_KEY from './api-keys';
+
 
 
 const requestCity = async (city) => {
     const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
-    const query = `?q=${city}&appid=${key}`
+    const query = `?q=${city}&appid=${WEATHER_API_KEY}`
 
     const response = await fetch(baseUrl+query);
     const data = await response.json();
---
+++ weatherApp/js/weather.js
@@ -0,0 +1,3 @@
+const WEATHER_API_KEY = 'f17daeaeca054442cd33e6f9028cb8a3';
+
+export default WEATHER_API_KEY;
